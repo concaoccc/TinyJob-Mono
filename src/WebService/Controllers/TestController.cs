3fc3ca693e9a0da4a3ba011ca61752a4fb0ede1c
concao@microsoft.com <concao@microsoft.com>
2024-06-08 19:07:24 +0800

    test controllers

---
+++ src/WebService/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        [HttpGet]
+        public IActionResult Error()
+        {
+            return Problem();
+        }
+
+        [Route("/error/test")]
+        [HttpGet]
+        public IActionResult Test()
+        {
+            throw new Exception("test");
+        }
+    }
+}
