edb5cf3e554531614f73ce45eb22fcfb1b7a2231
congcong <congcong@congcongs-MacBook-Pro.local>
2024-08-24 00:18:59 +0800

    add models and controllers

--- src/TinyJobApi/Models/User.cs
+++ src/TinyJobApi/Models/User.cs
@@ -4,7 +4,7 @@ namespace TinyJobApi.Models;
 
 public class User
 {
-    public required string UserName { get; set; }
+    public required string Name { get; set; }
     public required string Password { get; set; }
     public required string Email { get; set; }
 }
